401d8a1ecc29eb0e264b0adc6935f000eaaba65c
liyf <1010345121@qq.com>
2020-03-10 19:00:38 +0800

    支持中英文，页头调整

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,8 +38,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'overall_overview', label: 'Docs'},
+    {doc: 'overall_overview', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
